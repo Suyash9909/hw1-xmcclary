5b82796ff150fcec29b727e530321af6df490562
Xavion McClary-Fagan <xmcclary@stevens.edu>
2023-01-31 08:21:01 -0500

    Fixed Syntax Error

--- answer.py
+++ answer.py
@@ -71,7 +71,7 @@ def list_1D():
 
     # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
 
-    new_l = list[3:10]
+    new_l = l[3:10]
 
     return hoboken,hoboken_list, hoboken_first_item, l, new_l
 
